d23936300c8f57bd9afcf42117f784d716e57cf4
Vaibhav Pandey <vaibhavkpandeys@gmail.com>
2024-07-19 22:41:51 +0530

    added mock data and showed on the calander with api call

--- src/Scheduler.js
+++ src/Scheduler.js
@@ -1,32 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import axios from 'axios';
 
 const localizer = momentLocalizer(moment);
 
 const Scheduler = () => {
-  const [events, setEvents] = useState([
-    {
-      id: 0,
-      title: 'Scrum Meeting',
-      start: new Date(2024, 6, 1, 9, 30),
-      end: new Date(2024, 6, 1, 10, 30),
-      location: 'Office',
-    },
-    {
-      id: 1,
-      title: 'Project Deadline',
-      start: new Date(2024, 6, 3, 17, 0),
-      end: new Date(2024, 6, 3, 18, 0),
-    },
-    {
-      id: 1,
-      title: 'Project2',
-      start: new Date(2024, 6, 3, 17, 0),
-      end: new Date(2024, 6, 3, 18, 0),
-    },
-  ]);
+
+  const [events, setEvents] = useState([]);
+  
+  const getData = async()=>{
+    const response = await axios.get('/mockData.json')
+    setEvents(response.data)
+  }
+  
+  useEffect(()=>{
+    getData()
+  },[])
 
   return (
     <div style={{ height: '80vh' }}>
